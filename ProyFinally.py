d53752ec55764c9f09a4648e4394b7a318ba84da
DanielHincapieL <daniel.hincapie4@udea.edu.co>
2024-06-05 11:02:53 -0500

    Update ProyFinally.py

--- ProyFinally.py
+++ ProyFinally.py
@@ -4,10 +4,9 @@ import logging
 import platform
 import time
 from datetime import datetime
-import numpy as np  # Importa numpy para trabajar con valores NaN
+import numpy as np  
 
 def configurar_registro():
-    # Configuración del registro de eventos
     log_filename = 'procedimientos.log'
     logging.basicConfig(
         filename=log_filename,
@@ -16,7 +15,6 @@ def configurar_registro():
     )
 
 def registrar_encabezado():
-    # Encabezado del log
     user = os.getlogin()
     system_info = platform.uname()
     header = f"Usuario: {user}\nSistema Operativo: {system_info.system}\n" \
@@ -26,11 +24,9 @@ def registrar_encabezado():
     logging.info(header)
 
 def leer_datos_estudiantes(file_path):
-    # Leer el archivo Excel que contiene la información de los estudiantes
     return pd.read_excel(file_path)
 
 def leer_asignaturas(file_path):
-    # Leer el archivo Excel que contiene la información de las asignaturas
     asignaturas_df = pd.read_excel(file_path)
     asignaturas = {}
     for index, row in asignaturas_df.iterrows():
@@ -59,7 +55,7 @@ def calcular_horas_docente(creditos):
     elif creditos == 1:
         return 16
     else:
-        return 0  # Valor predeterminado si no coincide con ninguno de los casos
+        return 0 
 
 def calcular_horas_independiente(creditos):
     if creditos == 12:
@@ -73,14 +69,11 @@ def calcular_horas_independiente(creditos):
     elif creditos == 1:
         return 32
     else:
-        return 0  # Valor predeterminado si no coincide con ninguno de los casos
+        return 0 
 
 
 def crear_estructura_archivos(df, asignaturas, cupos_por_semestre, carpeta_principal):
-    # Verifica y maneja los valores NaN en el DataFrame
-    df.fillna('', inplace=True)  # Reemplaza NaN con una cadena vacía
-
-    # Crear las carpetas correspondientes para las asignaturas y los grupos
+    df.fillna('', inplace=True)  
     for asignatura, info in asignaturas.items():
         semestre = info['nivel']
         carpeta_semestre = os.path.join(carpeta_principal, f"Semestre {semestre}")
@@ -97,7 +90,7 @@ def crear_estructura_archivos(df, asignaturas, cupos_por_semestre, carpeta_princ
             ruta_grupo_csv = os.path.join(carpeta_asignatura, nombre_grupo + ".csv")
             ruta_grupo_xlsx = os.path.join(carpeta_asignatura, nombre_grupo + ".xlsx")
 
-            grupo = grupo.fillna('')  # Reemplaza NaN con una cadena vacía en el grupo
+            grupo = grupo.fillna('') 
 
             grupo.to_csv(ruta_grupo_csv, index=False)
             with open(ruta_grupo_csv, 'a') as f:
@@ -134,7 +127,6 @@ def crear_estructura_archivos(df, asignaturas, cupos_por_semestre, carpeta_princ
                     worksheet.write(row_num, 1, value)
 
 def resumen_final(contar_archivos, mover_archivos, renombrar_archivos):
-    # Resumen final en el log
     logging.info(f"\nResumen de procedimientos realizados:\n")
     logging.info(f"Total de archivos contados: {contar_archivos}")
     logging.info(f"Total de archivos movidos: {mover_archivos}")
@@ -144,16 +136,12 @@ def ejecutar_procedimientos():
     configurar_registro()
     registrar_encabezado()
 
-    # Definir la carpeta principal
     carpeta_principal = "Semestres"
 
-    # Leer los datos de los estudiantes
     df = leer_datos_estudiantes(pd.read_excel, 'https://github.com/DanielHincapieL/Proyecto-Milei/raw/5b623c2049906aa00239524585f2d8a9db98c3d5/Students.xlsx')
 
-    # Leer el diccionario de asignaturas desde el archivo Excel
     asignaturas = leer_asignaturas(pd.read_excel, 'https://github.com/DanielHincapieL/Proyecto-Milei/raw/5b623c2049906aa00239524585f2d8a9db98c3d5/asignaturas.xlsx')
 
-    # Diccionario de cupos por semestre
     cupos_por_semestre = {
         1: 30,
         2: 30,
@@ -171,7 +159,6 @@ def ejecutar_procedimientos():
     mover_archivos = 0
     renombrar_archivos = 0
 
-    # Crear la estructura de archivos
     crear_estructura_archivos(df, asignaturas, cupos_por_semestre, carpeta_principal)
 
     resumen_final(contar_archivos, mover_archivos, renombrar_archivos)
